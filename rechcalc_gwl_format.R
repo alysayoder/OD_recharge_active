d40d39d95e880830291b0626b25167bdc4c61ed6
Alysa <yoder.alysa@gmail.com>
2018-06-26 10:51:51 -0700

    Recharge data format function
    Used to reformat extracted data from 'allsets.csv' to make it ready to be used in rech_fxn.R function. Fixed issue where interpolations were being done with factors instead of numerics.

--- rechcalc_gwl_format.R
+++ rechcalc_gwl_format.R
@@ -4,7 +4,7 @@
 rechcalc_gwl_format <- function(hilodata) {
 rownames(hilodata) <- NULL
 colnames(hilodata) <- NULL
-hilodata <- evhi14[-1,]
+hilodata <- hilodata[-1,]
 
   label <- read.csv(here('data', "MW_label_allsets.csv"), header = F) #get lat long data, already in order of allsets.csv wells
   label[3] <- NULL #removes well ID text column, leaving only lat long coords
@@ -13,5 +13,7 @@ hilodata <- data.frame(c(label,hilodata))
 colnames(hilodata) <- NULL
 rownames(hilodata) <- NULL
 colnames(hilodata) <- c("lat", "long", "level")  
+hilodata$level <- as.numeric(as.character(hilodata$level))
+return(data.frame(hilodata))
 }
   
